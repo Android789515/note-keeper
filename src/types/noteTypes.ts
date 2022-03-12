31647f0f535e88b5b29c7f6a3df4710c8764e7db
Android789515 <derflug@mailfence.com>
2022-03-11 21:40:45 -0600

    Added more note colors.

--- src/types/noteTypes.ts
+++ src/types/noteTypes.ts
@@ -2,7 +2,11 @@ import { ID, Text, Color } from './types'
 
 export enum NoteColors {
     default = '#fff476',
-    blue = '#76a4ff'
+    green = '#81ff76',
+    blue = '#76a4ff',
+    purple = '#767bff',
+    pink = '#ed76ff',
+    red = '#ff7676'
 }
 
 export interface Note {
