0adadc16ddd15abd5fcb9586f1042bbe96c5528e
Android789515 <derflug@mailfence.com>
2022-05-17 08:10:46 -0500

    Updated useLayoutChanger to check the viewport size when it's created.

--- src/components/notes/useLayoutChanger.ts
+++ src/components/notes/useLayoutChanger.ts
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 
+const isWidthMobileSize = () => {
+    const viewportWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+
+    return viewportWidth < 600
+}
+
 const useLayoutChanger = () => {
-    const [ isMobileLayout, setIsMobileLayout ] = useState(false)
+    const [ isMobileLayout, setIsMobileLayout ] = useState(isWidthMobileSize())
 
     const isLayoutMobile = () => isMobileLayout
 
     const setLayout = () => {
-        const viewportWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-
-        if (viewportWidth < 600) {
+        if (isWidthMobileSize()) {
             setIsMobileLayout(true)
         } else {
             setIsMobileLayout(false)
